05fd347f67c1d9fba5852dad04a2c5e40dc5fd26
HLong <info.longth@gmail.com>
2023-01-11 13:12:36 +0700

    update read me

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,4 +15,4 @@ mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
     .sourceMaps();
 
-mix.copyDirectory('resources/images', 'public/images')
+mix.copyDirectory('resources/images', 'public/images');
